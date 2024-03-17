3e2e04fd863e2d8910b3d26a8ad691c5cd4692b6
Raul Andrade <raul10.3@hotmail.com>
2024-03-17 15:57:57 -0300

    refactor: let this file more generic

--- src/file_utils.rs
+++ src/file_utils.rs
@@ -1,32 +1,16 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, Result};
 use std::{fs, io::Write, path::Path};
 
-use crate::creator::SubStructure;
-
-// TODO: let this function generic with generic type
-pub fn create_folders(path: &Path, config: &SubStructure) -> Result<()> {
-    for (folder_name, folder_config) in config {
-        let folder_path = path.join(folder_name);
-        fs::create_dir_all(&folder_path)
-            .with_context(|| format!("Failed to create folder '{}'", folder_path.display()))?;
-        let file = &folder_config.file;
-        create_files(&folder_path.join(file)).with_context(|| {
-            format!(
-                "Failed to create file in folder '{}'",
+pub fn create_folder(folder_path: &Path) -> Result<()> {
+    match fs::create_dir_all(&folder_path) {
+        Ok(r) => r,
+        Err(_) => {
+            return Err(anyhow!(format!(
+                "Failed to create folder '{}'",
                 folder_path.display()
-            )
-        })?;
-    }
-
-    Ok(())
-}
-
-pub fn create_files(file_path: &Path) -> Result<()> {
-    let mut file = fs::File::create(file_path)
-        .with_context(|| format!("Failed to create file '{}'", file_path.display()))?;
-    let content = "Hello, Rust!";
-    file.write_all(content.as_bytes())
-        .with_context(|| format!("Failed to write content to file '{}'", file_path.display()))?;
+            )))
+        }
+    };
 
     Ok(())
 }
