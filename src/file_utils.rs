8cd4941661102998d1792a5b1d20980e6cbf348d
Raul Andrade <raul10.3@hotmail.com>
2024-03-17 22:31:02 -0300

    refactor: improve file utils error handling

--- src/file_utils.rs
+++ src/file_utils.rs
@@ -2,39 +2,24 @@ use anyhow::{anyhow, Result};
 use std::{fs, io::Write, path::Path};
 
 pub fn create_folder(folder_path: &Path) -> Result<()> {
-    match fs::create_dir_all(&folder_path) {
-        Ok(r) => r,
-        Err(_) => {
-            return Err(anyhow!(format!(
-                "Failed to create folder '{}'",
-                folder_path.display()
-            )))
-        }
-    };
-
-    Ok(())
+    fs::create_dir_all(folder_path).map_err(|err| {
+        anyhow!(
+            "Failed to create folder '{}': {}",
+            folder_path.display(),
+            err
+        )
+    })
 }
 
 pub fn create_file(file_path: &Path, content: String) -> Result<usize> {
-    let mut file = match fs::File::create(&file_path) {
-        Ok(f) => f,
-        Err(_) => {
-            return Err(anyhow!(format!(
-                "Failed to create file '{}'",
-                file_path.display()
-            )))
-        }
-    };
+    let mut file = fs::File::create(file_path)
+        .map_err(|err| anyhow!("Failed to create file '{}': {}", file_path.display(), err))?;
 
-    let result = match file.write(content.as_bytes()) {
-        Ok(r) => r,
-        Err(_) => {
-            return Err(anyhow!(format!(
-                "Failed to write content to file '{}'",
-                file_path.display()
-            )));
-        }
-    };
-
-    Ok(result)
+    file.write(content.as_bytes()).map_err(|err| {
+        anyhow!(
+            "Failed to write content to file '{}': {}",
+            file_path.display(),
+            err
+        )
+    })
 }
